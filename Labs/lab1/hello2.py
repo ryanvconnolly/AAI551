4732af934b1dd6a657f74ec94a4a708ff49c1534
Ryan Connolly <95229435+ryanvconnolly@users.noreply.github.com>
2024-09-03 20:38:42 -0400

    Add files via upload

---
+++ Labs/lab1/hello2.py
@@ -0,0 +1,6 @@
+# Author: Ryan Connolly
+# Date: September 3, 2024
+# Description: Prompts the user for their name and prints a message with the input.
+
+username = input("Please enter your name: ")
+print(f"hello {username}")
---
+++ Labs/lab1/hello3.py
@@ -0,0 +1,8 @@
+# Author: Ryan Connolly
+# Date: September 3. 2024
+# Description: Prompts user for name and age and prints message with input.
+
+username = input("Please enter you name: ")
+userAge = input("And now your age: ")
+
+print(f"hi {username}, you are {userAge} years old.")
